7bb31f40214491f7717c0a527f57a25c6c48bb5f
Jim Barnett <james.barnett.jr@gmail.com>
2024-07-31 22:10:56 -0500

    Updated site.

--- main.js
+++ main.js
@@ -1,16 +1,2 @@
-import './style.css'
-
-document.querySelector('#app').innerHTML = `
-  <div>
-    <img src="./images/image-jeremy.png" class="logo" alt="test" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+import "./style.css";
 
